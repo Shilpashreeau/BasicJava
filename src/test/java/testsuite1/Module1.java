531cc4073f6b06ce3f300e323198ebbdb790cd27
Shilpashreeau <shilpashreehebbar@gmail.com>
2022-11-28 18:38:20 -0500

    second commit

---
+++ src/test/java/testsuite1/Module1.java
@@ -0,0 +1,20 @@
+package testsuite1;
+
+import org.testng.annotations.Test;
+
+public class Module1 {
+	@Test
+	public void test01{
+		System.out.println("This is test case 1....");
+	}
+	@Test
+	public void test02{
+		System.out.println("This is test case 2....");
+	}
+	@Test
+	public void test03{
+		System.out.println("This is test case 3....");
+	}
+
+
+}
---
+++ target/test-classes/Module2.java
@@ -0,0 +1,15 @@
+
+public class Module2 {
+	
+	public void test04{
+		System.out.println("This is test case 4....");
+	}
+	public void test05{
+		System.out.println("This is test case 5....");
+	}
+
+	public void test06{
+		System.out.println("This is test case 6....");
+	}
+
+}
